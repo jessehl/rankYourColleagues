24d68f6fe8885f6fc1a5c5b86741527a2f081996
jessehl <j.loor@hotmail.com>
2021-12-18 16:21:58 +0100

    improve readability

--- src/main/scala/Score.scala
+++ src/main/scala/Score.scala
@@ -4,57 +4,67 @@ import scala.collection.{MapView, View}
 
 object Score {
 
-  // e.g. 'Person 1'
+  // E.g. 'Person 1'
   type Name = String 
   type Names = Seq[Name]
 
-  // e.g. 'person.1@example.nl'
+  // E.g. 'person.1@example.nl'
   type Contestant = String 
 
   type Points = Int 
 
-  // e.g. 'Who is most likely to ... ?'
+  // E.g. 'Who is most likely to ... ?'
   type Question = String 
 
 
   /* Returns the total Points earned per Contestant. */
   def compute(answers: Seq[Answer]): List[(Contestant, Points)] = {
 
-    // Spot duplicate Answers (answering the same question more than once). 
-    val duplicateAnswers: View[Answer] =
-      answers
-        .groupBy(_.contestant)
-        .mapValues(_.groupBy(_.question))
-        .flatMap(_._2.values)
-        .filter(answers => answers.length != 1)
-        .flatten
+    // Google Forms should enforce one answer per question, but better safe than sorry.
+    assertNoDuplicates(answers)
 
-    // Raise error if there are duplicates. 
-    // This constraint should be enforced by Google Forms (but better safe than sorry).
-    assert(duplicateAnswers.isEmpty, s"duplicate answers found: ${duplicateAnswers}")
-    
     // Filter out invalid Answers (i.e. more than 3 names).
     val (validAnswers, invalidAnswers) = answers.partition(_.names.length <= 3)
     invalidAnswers.foreach(answer => println(s"invalid answer: $answer"))
-  
+    
     // Get points awarded per Question, per Name. 
     val points: MapView[Question, MapView[Name, Points]] = 
       validAnswers
         .groupBy(answer => answer.question)
-        .mapValues(answers => answers
-          .flatMap(answer => answer.names)
-            .groupBy(identity)
-            .mapValues(names => names.length)
-        )
+        .mapValues(answers => computePointsPerQuestion(answers))
+
+    def computePointsForContestant(answers: Seq[Answer]): Points = answers
+      .flatMap(answer => answer.names.map(name => points(answer.question)(name)))
+      .sum
 
     // Return points earned per Contestant. 
     validAnswers
-      .groupBy(_.contestant) 
-      .mapValues(answers => answers
-        .flatMap(answer => answer.names
-          .map(name => points(answer.question)(name))
-        ).sum
-      ).toList
+      .groupBy(answer => answer.contestant) 
+      .mapValues(answers => computePointsForContestant(answers))
+      .toList
   }
+
+
+
+  /* Raises error when multiple contestants answered the same question more than once. */
+  private def assertNoDuplicates(answers: Seq[Answer]) = {
+    val duplicates = answers
+      .groupBy(_.contestant)
+      .mapValues(_.groupBy(_.question))
+      .flatMap(_._2.values)
+      .filter(answers => answers.length != 1)
+      .flatten
+
+    assert(duplicates.isEmpty, s"duplicate answers found: ${duplicates}")
+  }
+
+  private def computePointsPerQuestion(answers: Seq[Answer]): MapView[Name, Points] = {
+    answers
+      .flatMap(answer => answer.names)
+      .groupBy(identity)
+      .mapValues(names => names.length)
+    }
+
+
 
 }
--- src/main/scala/Tsv.scala
+++ src/main/scala/Tsv.scala
@@ -6,11 +6,9 @@ import scala.io.Source
 private object Tsv {
 
 
-/* 
-Returns tab-separated values file as a Seq of Maps.
+/* Returns tab-separated values file as a Seq of Maps.
 
-The assumption is that the values in the file don't contain tabs or newlines.
-*/
+The assumption is that the values in the file don't contain tabs or newlines. */
 def parse(filename: String): Seq[Map[String, String]] = {
     val lines = Source.fromFile(filename, "utf-8").getLines.toList
     val header = lines.head.split("\t")
